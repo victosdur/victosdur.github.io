8fbb1bb79da23290c9bd8f798503f9364a601dd5
victosdur77 <victortoscano21@gmail.com>
2024-04-16 10:35:59 +0200

    Add publications

---
+++ publications/script.js
@@ -0,0 +1,26 @@
+function toggleListPreprint() {
+    var lista = document.getElementById("listaPreprint");
+    if (lista.style.display === "none") {
+      lista.style.display = "block";
+    } else {
+      lista.style.display = "none";
+    }
+  }
+  
+function toggleList2024() {
+    var lista = document.getElementById("lista2024");
+    if (lista.style.display === "none") {
+      lista.style.display = "block";
+    } else {
+      lista.style.display = "none";
+    }
+  }
+
+function toggleListCollab() {
+    var lista = document.getElementById("listaCollab");
+    if (lista.style.display === "none") {
+      lista.style.display = "block";
+    } else {
+      lista.style.display = "none";
+    }
+  }
